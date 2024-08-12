ffbc78b8b14e3711c33a99c7b0f31d47a03e496c
Jim Lind <spoon.vw@gmail.com>
2024-08-11 21:52:50 -0400

    I am kind of lost now

--- src/main/java/jimlind/filmlinkd/Main.java
+++ src/main/java/jimlind/filmlinkd/Main.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
     public static void main(String[] args) {
+        System.err.println("I am kind of lost now");
         System.out.println("jimlind.filmlinkd main:main");
 
         Config config = new Config();
