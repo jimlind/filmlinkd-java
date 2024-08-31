9498e90529a1f82f216b60afc865d13cef56c580
Jim Lind <spoon.vw@gmail.com>
2024-08-31 09:32:34 -0400

    fix: make the queue less garbage
    I think the arraylist capacity is what is making it use up more memory over time.
    Also I don't have to build my own lock, Java let me.

--- src/main/java/jimlind/filmlinkd/Queue.java
+++ src/main/java/jimlind/filmlinkd/Queue.java
@@ -1,13 +1,14 @@
 package jimlind.filmlinkd;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import org.springframework.stereotype.Component;
 
 import com.google.pubsub.v1.PubsubMessage;
 
 @Component
-// Qeue exists so that I can rate limit the amount of processing that happens.
+// Queue exists so that I can rate limit the amount of processing that happens.
 // If we let every PubSub event trigger some logic it can take over the
 // CPU really quickly.
 //
@@ -15,37 +16,27 @@ import com.google.pubsub.v1.PubsubMessage;
 // this. I should go look that up and see if it does exist.
 public class Queue {
     public Boolean writeOnlyLock = true;
-    public Boolean getMethodSingleUseLock = false;
 
-    private ArrayList<PubsubMessage> messageList = new ArrayList<PubsubMessage>();
-    private ArrayList<Integer> fetchIdList = new ArrayList<Integer>();
+    private final LinkedList<PubsubMessage> messageList = new LinkedList<PubsubMessage>();
+    private final ArrayList<Integer> fetchIdList = new ArrayList<Integer>();
 
     public void set(PubsubMessage message) {
         messageList.add(message);
     }
 
-    public PubsubMessage get(Integer fetchClientId, Integer fetchClientTotal) throws Exception {
-        // A dumb way to lock the method, but works well enough
-        if (this.getMethodSingleUseLock == true) {
-            throw new Exception("Locked for Single Use");
-        }
-        this.getMethodSingleUseLock = true;
-
+    public synchronized PubsubMessage get(Integer fetchClientId, Integer fetchClientTotal) throws Exception {
         // Check if the specific ID was used for fetching and set it otherwise
         if (this.fetchIdList.contains(fetchClientId)) {
-            this.getMethodSingleUseLock = false;
             throw new Exception("Already Fetched this Resource");
         }
 
         // Don't allow reading until all fetching clients have connected
-        if (writeOnlyLock == true) {
-            this.getMethodSingleUseLock = false;
+        if (writeOnlyLock) {
             throw new Exception("Not Ready");
         }
 
         // Don't return anything if nothing to return
-        if (messageList.size() == 0) {
-            this.getMethodSingleUseLock = false;
+        if (messageList.isEmpty()) {
             throw new Exception("Nothing Available");
         }
 
@@ -53,16 +44,14 @@ public class Queue {
         this.fetchIdList.add(fetchClientId);
 
         // Get the first message from the queue
-        PubsubMessage message = messageList.get(0);
+        PubsubMessage message = messageList.getFirst();
 
         // Remove the first message because it's been fetched by all parties
         if (this.fetchIdList.size() == fetchClientTotal) {
-            messageList.remove(0);
+            messageList.removeFirst();
             this.fetchIdList.clear();
         }
-
-        // Unlock the method and return
-        this.getMethodSingleUseLock = false;
+        
         return message;
     }
 }
