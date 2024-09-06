324ece5bb7a95d092e23f24a21d01ea6c844a1b9
Jim Lind <spoon.vw@gmail.com>
2024-09-06 07:54:33 -0400

    feat: Kill process after 15 minutes
    A test to see if the VM fully reboots and follows settings.

--- src/main/java/jimlind/filmlinkd/Main.java
+++ src/main/java/jimlind/filmlinkd/Main.java
@@ -5,11 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 @Slf4j
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         log.info("Starting Things Up!");
         SpringApplication.run(Main.class, args);
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            public void run() {
+                System.exit(-1);
+            }
+        };
+        timer.schedule(task, 15*60*1000);
     }
 }
