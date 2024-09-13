4456d71b44414325c7935c3f421076495f951067
Jim Lind <spoon.vw@gmail.com>
2024-09-12 22:17:27 -0400

    fix: remove the timer that kills the process

--- src/main/java/jimlind/filmlinkd/Main.java
+++ src/main/java/jimlind/filmlinkd/Main.java
@@ -4,22 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 @Slf4j
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         log.info("Starting Things Up!");
         SpringApplication.run(Main.class, args);
-
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
-            public void run() {
-                System.exit(-1);
-            }
-        };
-        timer.schedule(task, 15*60*1000);
     }
 }
