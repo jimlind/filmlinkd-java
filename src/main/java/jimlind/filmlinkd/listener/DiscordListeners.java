d8d3f822f0a85801a9932c26a905e7dc6d0ce3e9
Jim Lind <spoon.vw@gmail.com>
2024-09-13 09:37:55 -0400

    feat: place for send success to exist
    also code formatting. Sorry.

--- src/main/java/jimlind/filmlinkd/listener/DiscordListeners.java
+++ src/main/java/jimlind/filmlinkd/listener/DiscordListeners.java
@@ -3,18 +3,19 @@ package jimlind.filmlinkd.listener;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.gson.GsonBuilder;
 import com.google.pubsub.v1.PubsubMessage;
-import jimlind.filmlinkd.factory.messageEmbed.DiaryEntryEmbedFactory;
-import jimlind.filmlinkd.system.google.PubSubQueue;
 import jimlind.filmlinkd.factory.UserFactory;
+import jimlind.filmlinkd.factory.messageEmbed.DiaryEntryEmbedFactory;
 import jimlind.filmlinkd.model.Message;
 import jimlind.filmlinkd.model.User;
 import jimlind.filmlinkd.system.google.FirestoreManager;
+import jimlind.filmlinkd.system.google.PubSubQueue;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.internal.entities.ReceivedMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -26,95 +27,98 @@ import java.util.TimerTask;
 @Component
 @Slf4j
 public class DiscordListeners extends ListenerAdapter {
-    @Autowired
-    private FirestoreManager firestoreManager;
-    @Autowired
-    private DiaryEntryEmbedFactory diaryEntryEmbedFactory;
-    @Autowired
-    private PubSubQueue pubSubQueue;
-    @Autowired
-    private UserFactory userFactory;
+  @Autowired private FirestoreManager firestoreManager;
+  @Autowired private DiaryEntryEmbedFactory diaryEntryEmbedFactory;
+  @Autowired private PubSubQueue pubSubQueue;
+  @Autowired private UserFactory userFactory;
 
-    @Override
-    public void onReady(ReadyEvent e) {
-        JDA jda = e.getJDA();
+  @Override
+  public void onReady(ReadyEvent e) {
+    JDA jda = e.getJDA();
 
-        ShardManager manager = jda.getShardManager();
-        // There is surely a cleaner way to do this in a filter or something but
-        // whatever. This works well enough.
-        boolean shardsLoggingIn = false;
-        for (var entry : manager.getStatuses().entrySet()) {
-            if (entry.getValue() == JDA.Status.LOGGING_IN) {
-                shardsLoggingIn = true;
-                break;
+    ShardManager manager = jda.getShardManager();
+    // There is surely a cleaner way to do this in a filter or something but
+    // whatever. This works well enough.
+    boolean shardsLoggingIn = false;
+    for (var entry : manager.getStatuses().entrySet()) {
+      if (entry.getValue() == JDA.Status.LOGGING_IN) {
+        shardsLoggingIn = true;
+        break;
+      }
+    }
+    // When shards are logging in (true) the write only lock stays on (true)
+    pubSubQueue.writeOnlyLock = shardsLoggingIn;
+
+    Timer timer = new Timer();
+    TimerTask task =
+        new TimerTask() {
+          // Fix this later please....
+          // Probably need to do the thing where I pass the scope to this method properly.
+          // I don't like not being able to use "this.queue" or "this.messageUtility"
+          public void run() {
+            // Try to grab something off the queue, it is expected that it will throw an
+            // exception if there isn't anything available or if all the shards aren't
+            // loaded.
+            String data;
+            try {
+              PubsubMessage result =
+                  pubSubQueue.get(jda.getShardInfo().getShardId(), manager.getShardsTotal());
+              data = result.getData().toStringUtf8();
+            } catch (Exception e) {
+              return;
             }
-        }
-        // When shards are logging in (true) the the write only lock stays on (true)
-        pubSubQueue.writeOnlyLock = shardsLoggingIn;
 
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
-            // Fix this later please....
-            // Probably need to do the thing where I pass the scope to this method properly.
-            // I don't like not being able to use "this.queue" or "this.messageUtility"
-            public void run() {
-                // Try to grab something off the queue, it is expected that it will throw an
-                // exception if there isn't anything available or if all the shards aren't
-                // loaded.
-                String data;
-                try {
-                    PubsubMessage result = pubSubQueue.get(jda.getShardInfo().getShardId(), manager.getShardsTotal());
-                    data = result.getData().toStringUtf8();
-                } catch (Exception e) {
-                    return;
-                }
-
-                User user = null;
-                Message message = new GsonBuilder().create().fromJson(data, Message.class);
-                try {
-                    QueryDocumentSnapshot userDocument = firestoreManager.getUserDocument(message.entry.userLid);
-                    user = userFactory.createFromDocument(userDocument);
-                } catch (Exception e) {
-                    log.warn("Invalid user [{}] passed in PubSub message", message.entry.userLid);
-                }
-                ArrayList<String> channelList = getChannelListFromMessage(message);
-
-                for (String channelId : channelList) {
-                    try {
-                        GuildMessageChannel channel = jda.getChannelById(GuildMessageChannel.class, channelId);
-                        if (channel != null && user != null) {
-                            channel.sendMessageEmbeds(diaryEntryEmbedFactory.create(message, user)).queue();
-                        }
-                    } catch (Exception e) {
-                        String name = message.entry.userName;
-                        String film = message.entry.filmTitle;
-                        log.info("Unable to write message for [{}] [{}] to [{}]", name, film, channelId, e);
-                        log.info("Message write failure", e);
-                        log.error("e: ", e);
-                    }
-                }
+            User user = null;
+            Message message = new GsonBuilder().create().fromJson(data, Message.class);
+            try {
+              QueryDocumentSnapshot userDocument =
+                  firestoreManager.getUserDocument(message.entry.userLid);
+              user = userFactory.createFromDocument(userDocument);
+            } catch (Exception e) {
+              log.warn("Invalid user [{}] passed in PubSub message", message.entry.userLid);
             }
+            ArrayList<String> channelList = getChannelListFromMessage(message);
+
+            for (String channelId : channelList) {
+              try {
+                GuildMessageChannel channel =
+                    jda.getChannelById(GuildMessageChannel.class, channelId);
+                if (channel != null && user != null) {
+                  channel
+                      .sendMessageEmbeds(diaryEntryEmbedFactory.create(message, user))
+                      .queue((recievedMessage) -> sendSuccess((ReceivedMessage) recievedMessage));
+                }
+              } catch (Exception e) {
+                String name = message.entry.userName;
+                String film = message.entry.filmTitle;
+                log.info("Unable to write message for [{}] [{}] to [{}]", name, film, channelId, e);
+              }
+            }
+          }
         };
-        // I can probably make this a lot shorter for actual use
-        timer.scheduleAtFixedRate(task, 0, 1000);
+    // I can probably make this a lot shorter for actual use
+    timer.scheduleAtFixedRate(task, 0, 1000);
+  }
+
+  private void sendSuccess(ReceivedMessage receivedMessage) {
+    log.info("The Message Was Successfully Sent - Log it in the Database.");
+  }
+
+  private ArrayList<String> getChannelListFromMessage(Message message) {
+    ArrayList<String> channelList = new ArrayList<String>();
+    if (!message.channelId.isBlank()) {
+      channelList.add(message.channelId);
+      return channelList;
     }
 
-    private ArrayList<String> getChannelListFromMessage(Message message) {
-        ArrayList<String> channelList = new ArrayList<String>();
-        if (!message.channelId.isBlank()) {
-            channelList.add(message.channelId);
-            return channelList;
-        }
-
-        try {
-            QueryDocumentSnapshot document = this.firestoreManager.getUserDocument(message.entry.userLid);
-            User user = this.userFactory.createFromDocument(document);
-            return user.getChannelList();
-        } catch (Exception e) {
-            log.info("Unable to fetch channel list from user", e);
-        }
-
-        return channelList;
+    try {
+      QueryDocumentSnapshot document = this.firestoreManager.getUserDocument(message.entry.userLid);
+      User user = this.userFactory.createFromDocument(document);
+      return user.getChannelList();
+    } catch (Exception e) {
+      log.info("Unable to fetch channel list from user", e);
     }
 
+    return channelList;
+  }
 }
