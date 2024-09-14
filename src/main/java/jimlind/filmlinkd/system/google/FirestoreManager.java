1aa157b828a2a045c2fc492c72adfd142615b0d5
Jim Lind <spoon.vw@gmail.com>
2024-09-14 17:00:55 -0400

    feat: add updateUserPrevious method for FireStore
    This includes an automatic formatting so things look worse than they are
    Right now everything is about the user so we'll need to split this up eventually I assume

--- src/main/java/jimlind/filmlinkd/system/google/FirestoreManager.java
+++ src/main/java/jimlind/filmlinkd/system/google/FirestoreManager.java
@@ -5,7 +5,12 @@ import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.firestore.FirestoreOptions.Builder;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
+import java.time.Instant;
+import java.util.ArrayList;
 import jimlind.filmlinkd.Config;
+import jimlind.filmlinkd.factory.UserFactory;
+import jimlind.filmlinkd.model.User;
+import jimlind.filmlinkd.system.letterboxd.LidComparer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,23 +19,56 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class FirestoreManager {
 
-    private final Config config;
-    private final com.google.cloud.firestore.Firestore db;
+  private final Config config;
+  private final UserFactory userFactory;
+  private final com.google.cloud.firestore.Firestore db;
 
-    @Autowired
-    public FirestoreManager(Config config) {
-        this.config = config;
+  @Autowired
+  public FirestoreManager(Config config, UserFactory userFactory) {
+    this.config = config;
+    this.userFactory = userFactory;
 
-        Builder builder = FirestoreOptions.getDefaultInstance().toBuilder()
-                .setProjectId(this.config.getGoogleProjectId());
-        FirestoreOptions firestoreOptions = builder.build();
-        this.db = firestoreOptions.getService();
+    Builder builder =
+        FirestoreOptions.getDefaultInstance().toBuilder()
+            .setProjectId(this.config.getGoogleProjectId());
+    FirestoreOptions firestoreOptions = builder.build();
+    this.db = firestoreOptions.getService();
+  }
+
+  public QueryDocumentSnapshot getUserDocument(String userLID) throws Exception {
+    String collectionId = this.config.getFirestoreCollectionId();
+
+    ApiFuture<QuerySnapshot> query =
+        this.db.collection(collectionId).whereEqualTo("letterboxdId", userLID).limit(1).get();
+    return query.get().getDocuments().get(0);
+  }
+
+  public boolean updateUserPrevious(
+      String userLID, String diaryLID, long diaryPublishedDate, String diaryURI) {
+    try {
+      QueryDocumentSnapshot snapshot = getUserDocument(userLID);
+      User user = this.userFactory.createFromSnapshot(snapshot);
+      ArrayList<String> previousList = user.previous.list;
+
+      if (LidComparer.compare(user.previous.lid, diaryLID) < 0) {
+        // This is the important data to update
+        // The scraping process uses it to determine which entries are new
+        user.previous.lid = diaryLID;
+        // This data is only used if I'm trying to debug publishing issues
+        user.updated = Instant.now().toEpochMilli();
+        user.previous.published = diaryPublishedDate;
+        user.previous.uri = diaryURI;
+        // This list is not a source of truth
+        // It will only contain LIDs that are the most recent when published
+        user.previous.list = LidComparer.buildMostRecentList(previousList, diaryLID, 10);
+      }
+
+      snapshot.getReference().update(user.toMap());
+    } catch (Exception e) {
+      log.error("Unable to update user", e);
+      return false;
     }
 
-    public QueryDocumentSnapshot getUserDocument(String userLID) throws Exception {
-        String collectionId = this.config.getFirestoreCollectionId();
-
-        ApiFuture<QuerySnapshot> query = this.db.collection(collectionId).whereEqualTo("letterboxdId", userLID).limit(1).get();
-        return query.get().getDocuments().get(0);
-    }
+    return true;
+  }
 }
