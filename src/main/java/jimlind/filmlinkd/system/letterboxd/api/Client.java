1837d777b27c2da823b8e419bc9591097f019bf9
Jim Lind <spoon.vw@gmail.com>
2024-10-14 00:00:09 -0400

    feat: build an API to get contributors
    no auth is needed for this

---
+++ src/main/java/jimlind/filmlinkd/system/letterboxd/api/Client.java
@@ -0,0 +1,28 @@
+package jimlind.filmlinkd.system.letterboxd.api;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
+
+@Component
+public class Client {
+  static final String BASE_URL = "https://api.letterboxd.com/api/v0/";
+
+  public <T> ResponseEntity<T> get(Function<UriBuilder, URI> uriFunction, Class<T> clazz) {
+    return this.buildClient()
+        .get()
+        .uri(uriFunction)
+        .acceptCharset(StandardCharsets.UTF_8)
+        .retrieve()
+        .toEntity(clazz)
+        .block();
+  }
+
+  private WebClient buildClient() {
+    return WebClient.builder().baseUrl(BASE_URL).build();
+  }
+}
---
+++ src/main/java/jimlind/filmlinkd/system/letterboxd/api/ContributorAPI.java
@@ -0,0 +1,29 @@
+package jimlind.filmlinkd.system.letterboxd.api;
+
+import java.net.URI;
+import java.util.function.Function;
+import jimlind.filmlinkd.system.letterboxd.model.LBSearchResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriBuilder;
+
+@Component
+public class ContributorAPI {
+  @Autowired private Client client;
+
+  public LBSearchResponse fetch(String searchTerm) {
+    Function<UriBuilder, URI> buildURI =
+        (UriBuilder uriBuilder) ->
+            uriBuilder
+                .path("/search/")
+                .queryParam("input", searchTerm)
+                .queryParam("include", "ContributorSearchItem")
+                .queryParam("perPage", 1)
+                .build();
+
+    ResponseEntity<LBSearchResponse> response = this.client.get(buildURI, LBSearchResponse.class);
+
+    return response != null ? response.getBody() : null;
+  }
+}
