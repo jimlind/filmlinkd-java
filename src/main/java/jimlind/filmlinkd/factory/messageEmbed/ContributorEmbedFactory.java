1c04f9da7a261a1e3463f76c957274b4f560fdba
Jim Lind <spoon.vw@gmail.com>
2024-10-14 00:00:40 -0400

    feat: use the API results in the slash command

---
+++ src/main/java/jimlind/filmlinkd/factory/messageEmbed/ContributorEmbedFactory.java
@@ -0,0 +1,48 @@
+package jimlind.filmlinkd.factory.messageEmbed;
+
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import jimlind.filmlinkd.system.discord.embedComponent.EmbedDescription;
+import jimlind.filmlinkd.system.letterboxd.model.LBContributionStatistics;
+import jimlind.filmlinkd.system.letterboxd.model.LBContributor;
+import jimlind.filmlinkd.system.letterboxd.model.LBLink;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContributorEmbedFactory {
+  public ArrayList<MessageEmbed> create(LBContributor contributor) {
+    EmbedBuilder embedBuilder = new EmbedBuilder();
+    embedBuilder.setTitle(contributor.name, String.format("https://boxd.it/%s", contributor.id));
+
+    List<String> linkStrings = new LinkedList<>();
+    for (LBLink link : contributor.links) {
+      String text = String.format("[%s](%s)", link.type, link.url);
+      linkStrings.add(text);
+    }
+    String joinedLinkStrings = String.join(" | ", linkStrings);
+
+    List<String> contributionStrings = new LinkedList<>();
+    for (LBContributionStatistics contribution : contributor.statistics.contributions) {
+      String text = String.format("**%s:** %s", contribution.type, contribution.filmCount);
+      contributionStrings.add(text);
+    }
+    String joinedContributionStrings = String.join("\n", contributionStrings);
+
+    List<String> descriptionElements =
+        Lists.newArrayList(joinedLinkStrings, joinedContributionStrings, contributor.bio);
+
+    descriptionElements.removeAll(Collections.singleton(null));
+    embedBuilder.setDescription(
+        new EmbedDescription(String.join("\n\n", descriptionElements)).build());
+
+    ArrayList<MessageEmbed> embedList = new ArrayList<>();
+    embedList.add(embedBuilder.build());
+
+    return embedList;
+  }
+}
--- src/main/java/jimlind/filmlinkd/system/discord/eventHandler/ContributorHandler.java
+++ src/main/java/jimlind/filmlinkd/system/discord/eventHandler/ContributorHandler.java
@@ -1,15 +1,42 @@
 package jimlind.filmlinkd.system.discord.eventHandler;
 
+import java.util.ArrayList;
+import jimlind.filmlinkd.factory.messageEmbed.ContributorEmbedFactory;
+import jimlind.filmlinkd.system.letterboxd.api.ContributorAPI;
+import jimlind.filmlinkd.system.letterboxd.model.LBContributor;
+import jimlind.filmlinkd.system.letterboxd.model.LBSearchResponse;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ContributorHandler implements Handler {
+  @Autowired private ContributorEmbedFactory contributorEmbedFactory;
+  @Autowired private ContributorAPI contributorAPI;
+
   public String getEventName() {
     return "contributor";
   }
 
   public void handleEvent(SlashCommandInteractionEvent event) {
-    event.reply("Nothing has been contributed.").queue();
+    event.deferReply().queue();
+
+    OptionMapping optionMapping = event.getInteraction().getOption("contributor-name");
+    if (optionMapping == null) {
+      event.getHook().sendMessage("Error").queue();
+      return;
+    }
+
+    LBSearchResponse searchResponse = this.contributorAPI.fetch(optionMapping.getAsString());
+    if (searchResponse.items.isEmpty()) {
+      event.getHook().sendMessage("No Results Found").queue();
+      return;
+    }
+
+    LBContributor contributor = searchResponse.items.get(0).contributor;
+    ArrayList<MessageEmbed> messageEmbedList = this.contributorEmbedFactory.create(contributor);
+    event.getHook().sendMessageEmbeds(messageEmbedList).queue();
   }
 }
