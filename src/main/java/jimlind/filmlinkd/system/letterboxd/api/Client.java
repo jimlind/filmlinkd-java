88ce52fcff140dc0d0f8f49259a860b7394cb32d
Jim Lind <spoon.vw@gmail.com>
2024-10-18 18:27:56 -0400

    feat: log exceptions from the web client

--- src/main/java/jimlind/filmlinkd/system/letterboxd/api/Client.java
+++ src/main/java/jimlind/filmlinkd/system/letterboxd/api/Client.java
@@ -4,12 +4,15 @@ import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.function.Function;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriBuilder;
 
 @Component
+@Slf4j
 public class Client {
   static final String BASE_URL = "https://api.letterboxd.com/api/v0/";
 
@@ -28,6 +31,14 @@ public class Client {
 
       return validateResponse(response);
     } catch (Exception e) {
+      UriBuilder b = new DefaultUriBuilderFactory().builder();
+      log.atError()
+          .setMessage("Error on WebClient GET")
+          .addKeyValue("exception", e)
+          .addKeyValue("uri", uriFunction.apply(b))
+          .addKeyValue("classOutput", inputClass)
+          .log();
+
       return null;
     }
   }
