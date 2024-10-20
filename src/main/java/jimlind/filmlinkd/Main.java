da9bce6f9eda31325a37d9ed7d130d7a6e2861d8
Jim Lind <spoon.vw@gmail.com>
2024-10-19 22:47:30 -0400

    chore: format file

--- src/main/java/jimlind/filmlinkd/Main.java
+++ src/main/java/jimlind/filmlinkd/Main.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
-        log.info("Starting Things Up!");
-        SpringApplication.run(Main.class, args);
-    }
+  public static void main(String[] args) {
+    log.info("Starting Things Up!");
+    SpringApplication.run(Main.class, args);
+  }
 }
