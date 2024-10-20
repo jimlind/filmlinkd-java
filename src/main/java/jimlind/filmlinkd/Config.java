522181cc04c034c108b3951602bf0ec0799b0ef2
Jim Lind <spoon.vw@gmail.com>
2024-10-19 22:18:54 -0400

    feat: add letterboxd api secret values
    includes a reformat

--- src/main/java/jimlind/filmlinkd/Config.java
+++ src/main/java/jimlind/filmlinkd/Config.java
@@ -12,66 +12,95 @@ import java.util.Properties;
 @Component
 @Slf4j
 public class Config {
-    private static final String GCP_PROJECT_ID_NAME = "googleCloudProjectId";
-    private static final String DISCORD_BOT_TOKEN_NAME = "discordBotToken";
-    private static final String PUBSUB_LOG_ENTRY_TOPIC_NAME = "logEntryTopicName";
-    private static final String PUBSUB_LOG_ENTRY_SUBSCRIPTION_NAME = "logEntrySubscriptionName";
-    private static final String FIRESTORE_COLLECTION_ID = "firesStoreCollectionId";
+  private static final String GCP_PROJECT_ID_NAME = "googleCloudProjectId";
+  private static final String DISCORD_BOT_TOKEN_NAME = "discordBotToken";
+  private static final String PUBSUB_LOG_ENTRY_TOPIC_NAME = "logEntryTopicName";
+  private static final String PUBSUB_LOG_ENTRY_SUBSCRIPTION_NAME = "logEntrySubscriptionName";
+  private static final String FIRESTORE_COLLECTION_ID = "firesStoreCollectionId";
+  private static final String LETTERBOXD_API_KEY = "letterboxdAPIKey";
+  private static final String LETTERBOXD_API_SHARED = "letterboxdAPIShared";
 
-    private static final Properties publicProperties = new Properties();
-    private static final Properties secretProperties = new Properties();
-    private static SecretManagerServiceClient secretManagerServiceClient;
+  private static final Properties publicProperties = new Properties();
+  private static final Properties secretProperties = new Properties();
+  private static SecretManagerServiceClient secretManagerServiceClient;
 
-    static {
-        String environment = System.getenv("FILMLINKD_ENVIRONMENT");
-        environment = environment != null ? environment : "";
+  static {
+    String environment = System.getenv("FILMLINKD_ENVIRONMENT");
+    environment = environment != null ? environment : "";
 
-        try {
-            String resourcesDir = environment.equals("PRODUCTION") ? "prod/" : "dev/";
-            InputStream stream = Config.class.getClassLoader()
-                    .getResourceAsStream(resourcesDir + "environment.properties");
+    try {
+      String resourcesDir = environment.equals("PRODUCTION") ? "prod/" : "dev/";
+      InputStream stream =
+          Config.class
+              .getClassLoader()
+              .getResourceAsStream(resourcesDir + "environment.properties");
 
-            publicProperties.load(stream);
-        } catch (Exception e) {
-            log.error("Error Loading Properties", e);
-            System.exit(1);
-        }
-
-        try {
-            secretManagerServiceClient = SecretManagerServiceClient.create();
-        } catch (Exception e) {
-            log.error("Error Creating Secret Manager Service Client", e);
-            System.exit(1);
-        }
-
-        secretProperties.setProperty(DISCORD_BOT_TOKEN_NAME,
-                getSecret(publicProperties.getProperty("discordBotTokenSecretName"),
-                        publicProperties.getProperty("discordBotTokenSecretVersion")));
+      publicProperties.load(stream);
+    } catch (Exception e) {
+      log.error("Error Loading Properties", e);
+      System.exit(1);
     }
 
-    public String getGoogleProjectId() {
-        return publicProperties.getProperty(GCP_PROJECT_ID_NAME);
+    try {
+      secretManagerServiceClient = SecretManagerServiceClient.create();
+    } catch (Exception e) {
+      log.error("Error Creating Secret Manager Service Client", e);
+      System.exit(1);
     }
 
-    public String getDiscordBotToken() {
-        return secretProperties.getProperty(DISCORD_BOT_TOKEN_NAME);
-    }
+    secretProperties.setProperty(
+        DISCORD_BOT_TOKEN_NAME,
+        getSecret(
+            publicProperties.getProperty("discordBotTokenSecretName"),
+            publicProperties.getProperty("discordBotTokenSecretVersion")));
 
-    public String getPubSubLogEntryTopicName() {
-        return publicProperties.getProperty(PUBSUB_LOG_ENTRY_TOPIC_NAME);
-    }
+    secretProperties.setProperty(
+        LETTERBOXD_API_KEY,
+        getSecret(
+            publicProperties.getProperty("letterboxdAPIKeySecretName"),
+            publicProperties.getProperty("letterboxdAPIKeySecretVersion")));
 
-    public String getPubSubLogEntrySubscriptionName() {
-        return publicProperties.getProperty(PUBSUB_LOG_ENTRY_SUBSCRIPTION_NAME);
-    }
+    secretProperties.setProperty(
+        LETTERBOXD_API_SHARED,
+        getSecret(
+            publicProperties.getProperty("letterboxdAPISharedSecretName"),
+            publicProperties.getProperty("letterboxdAPISharedSecretVersion")));
+  }
 
-    public String getFirestoreCollectionId() { return publicProperties.getProperty(FIRESTORE_COLLECTION_ID); }
+  public String getGoogleProjectId() {
+    return publicProperties.getProperty(GCP_PROJECT_ID_NAME);
+  }
 
-    private static String getSecret(String secretName, String secretVersion) {
-        String project = publicProperties.getProperty(GCP_PROJECT_ID_NAME);
-        SecretVersionName secretVersionName = SecretVersionName.of(project, secretName, secretVersion);
+  public String getDiscordBotToken() {
+    return secretProperties.getProperty(DISCORD_BOT_TOKEN_NAME);
+  }
 
-        AccessSecretVersionResponse secretResponse = secretManagerServiceClient.accessSecretVersion(secretVersionName);
-        return secretResponse.getPayload().getData().toStringUtf8();
-    }
+  public String getPubSubLogEntryTopicName() {
+    return publicProperties.getProperty(PUBSUB_LOG_ENTRY_TOPIC_NAME);
+  }
+
+  public String getPubSubLogEntrySubscriptionName() {
+    return publicProperties.getProperty(PUBSUB_LOG_ENTRY_SUBSCRIPTION_NAME);
+  }
+
+  public String getFirestoreCollectionId() {
+    return publicProperties.getProperty(FIRESTORE_COLLECTION_ID);
+  }
+
+  public String getLetterboxdApiKey() {
+    return secretProperties.getProperty(LETTERBOXD_API_KEY);
+  }
+
+  public String getLetterboxdApiShared() {
+    return secretProperties.getProperty(LETTERBOXD_API_SHARED);
+  }
+
+  private static String getSecret(String secretName, String secretVersion) {
+    String project = publicProperties.getProperty(GCP_PROJECT_ID_NAME);
+    SecretVersionName secretVersionName = SecretVersionName.of(project, secretName, secretVersion);
+
+    AccessSecretVersionResponse secretResponse =
+        secretManagerServiceClient.accessSecretVersion(secretVersionName);
+    return secretResponse.getPayload().getData().toStringUtf8();
+  }
 }
