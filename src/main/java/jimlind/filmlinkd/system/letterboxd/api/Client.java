eb41ddd44f2b0ee186ca2459a63db804cf4d4893
Jim Lind <spoon.vw@gmail.com>
2024-10-22 22:36:35 -0400

    feat: a client with proper authorization
    There is plenty of copy/paste between authorized and not authorized that should be resolved eventually.

--- src/main/java/jimlind/filmlinkd/system/letterboxd/api/Client.java
+++ src/main/java/jimlind/filmlinkd/system/letterboxd/api/Client.java
@@ -3,8 +3,13 @@ package jimlind.filmlinkd.system.letterboxd.api;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.time.Instant;
 import java.util.function.Function;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import jimlind.filmlinkd.Config;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,6 +19,8 @@ import org.springframework.web.util.UriBuilder;
 @Component
 @Slf4j
 public class Client {
+  @Autowired private Config config;
+
   static final String BASE_URL = "https://api.letterboxd.com/api/v0/";
 
   public <T> ResponseEntity<T> get(Function<UriBuilder, URI> uriFunction, Class<T> inputClass) {
@@ -43,6 +50,29 @@ public class Client {
     }
   }
 
+  public <T> ResponseEntity<T> getAuthorized(String path, Class<T> inputClass) {
+    String key = this.config.getLetterboxdApiKey();
+    String nonce = String.valueOf(java.util.UUID.randomUUID());
+    String now = String.valueOf(Instant.now().getEpochSecond());
+    String uri = path + String.format("?apikey=%s&nonce=%s&timestamp=%s", key, nonce, now);
+    String url = BASE_URL + uri;
+    String signature = this.buildSignature("GET", url);
+
+    ResponseEntity<T> response =
+        this.buildClient()
+            .get()
+            .uri(uri)
+            .header("User-Agent", "Filmlinkd - A Letterboxd Discord Bot")
+            .header("Authorization", "Signature " + signature)
+            .acceptCharset(StandardCharsets.UTF_8)
+            .retrieve()
+            .toEntity(inputClass)
+            .timeout(Duration.ofSeconds(6))
+            .block();
+
+    return validateResponse(response);
+  }
+
   private WebClient buildClient() {
     return WebClient.builder().baseUrl(BASE_URL).build();
   }
@@ -55,5 +85,24 @@ public class Client {
       return response;
     }
     return null;
+  }
+
+  private String buildSignature(String method, String url) {
+    try {
+      String shared = this.config.getLetterboxdApiShared();
+      Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+      SecretKeySpec secretKeySpec = new SecretKeySpec(shared.getBytes(), "HmacSHA256");
+      sha256_HMAC.init(secretKeySpec);
+      String data = method + "\u0000" + url + "\u0000"; // "�"
+      return bytesToHex(sha256_HMAC.doFinal(data.getBytes()));
+    } catch (Exception e) {
+      return "";
+    }
+  }
+
+  private static String bytesToHex(byte[] in) {
+    final StringBuilder builder = new StringBuilder();
+    for (final byte b : in) builder.append(String.format("%02x", b));
+    return builder.toString();
   }
 }
