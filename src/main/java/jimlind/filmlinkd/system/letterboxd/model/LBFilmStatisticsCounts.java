0948d80d2359424e296d46d2f20a8c858d532e4b
Jim Lind <spoon.vw@gmail.com>
2024-10-25 20:23:28 -0400

    fix: make sure that we can handle very big numbers

--- src/main/java/jimlind/filmlinkd/system/letterboxd/model/LBFilmStatisticsCounts.java
+++ src/main/java/jimlind/filmlinkd/system/letterboxd/model/LBFilmStatisticsCounts.java
@@ -2,10 +2,10 @@ package jimlind.filmlinkd.system.letterboxd.model;
 
 // https://api-docs.letterboxd.com/#/schemas/FilmStatisticsCounts
 public class LBFilmStatisticsCounts {
-  public int watches;
-  public int likes;
-  public int ratings;
-  public int fans;
-  public int lists;
-  public int reviews;
+  public long watches;
+  public long likes;
+  public long ratings;
+  public long fans;
+  public long lists;
+  public long reviews;
 }
