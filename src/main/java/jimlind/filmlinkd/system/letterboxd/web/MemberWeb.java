2013d4497ce34c3ffc021a5b8a7fd128e10493f3
Jim Lind <spoon.vw@gmail.com>
2024-10-28 08:59:24 -0400

    feat: Method that gets Letterboxd ID from username
    The search doesn't actually work for common names like "august" which sucks.

---
+++ src/main/java/jimlind/filmlinkd/system/letterboxd/web/MemberWeb.java
@@ -0,0 +1,44 @@
+package jimlind.filmlinkd.system.letterboxd.web;
+
+import java.time.Duration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class MemberWeb {
+  public String getMemberLIDFromUsername(String username) {
+    String url = String.format("https://letterboxd.com/%s/", username);
+
+    try {
+      ResponseEntity<Void> response =
+          WebClient.builder()
+              .baseUrl(url)
+              .build()
+              .get()
+              .retrieve()
+              .toBodilessEntity()
+              .timeout(Duration.ofSeconds(6))
+              .block();
+      return validateResponse(response);
+    } catch (Exception e) {
+      return "";
+    }
+  }
+
+  private String validateResponse(ResponseEntity<Void> response) {
+    if (response == null) {
+      return "";
+    }
+
+    HttpHeaders headers = response.getHeaders();
+    String letterboxdType = headers.getFirst("x-letterboxd-type");
+    if (letterboxdType != null && !letterboxdType.equals("Member")) {
+      return "";
+    }
+
+    String letterboxdId = headers.getFirst("x-letterboxd-identifier");
+    return letterboxdId != null ? letterboxdId : "";
+  }
+}
