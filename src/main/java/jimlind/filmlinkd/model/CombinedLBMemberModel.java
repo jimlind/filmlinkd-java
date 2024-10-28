74f4c3a903e1070c1d08ad0e2f47c1ee431986b7
Jim Lind <spoon.vw@gmail.com>
2024-10-28 09:27:27 -0400

    chore: remove member summary from search
    Since I don't want to search for users there are dead code paths to delete.

--- src/main/java/jimlind/filmlinkd/model/CombinedLBMemberModel.java
+++
@@ -1,9 +0,0 @@
-package jimlind.filmlinkd.model;
-
-import jimlind.filmlinkd.system.letterboxd.model.LBMember;
-import jimlind.filmlinkd.system.letterboxd.model.LBMemberSummary;
-
-public class CombinedLBMemberModel {
-  public LBMember member;
-  public LBMemberSummary memberSummary;
-}
--- src/main/java/jimlind/filmlinkd/system/letterboxd/api/MemberAPI.java
+++ src/main/java/jimlind/filmlinkd/system/letterboxd/api/MemberAPI.java
@@ -1,6 +1,5 @@
 package jimlind.filmlinkd.system.letterboxd.api;
 
-import jimlind.filmlinkd.model.CombinedLBMemberModel;
 import jimlind.filmlinkd.system.letterboxd.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,46 +9,14 @@ import org.springframework.stereotype.Component;
 public class MemberAPI {
   @Autowired private Client client;
 
-  public CombinedLBMemberModel fetch(String username) {
-    // Search for the member by name
-    String uriTemplate = "search/?input=%s&include=%s&perPage=%s";
-    String searchPath = String.format(uriTemplate, username, "MemberSearchItem", 4);
-    ResponseEntity<LBSearchResponse> searchResponse =
-        this.client.get(searchPath, LBSearchResponse.class);
-
-    // Exit if no worthwhile responses from the client
-    if (searchResponse == null
-        || searchResponse.getBody() == null
-        || searchResponse.getBody().items.isEmpty()) {
-      return null;
-    }
-
-    // Ensure that search results are correct
-    LBMemberSummary memberSummary = null;
-    for (LBAbstractSearchItem searchItem : searchResponse.getBody().items) {
-      if (username.equals(searchItem.member.username)) {
-        memberSummary = searchItem.member;
-      }
-      break;
-    }
-
-    // Exit if no matching users in the search
-    if (memberSummary == null) {
-      return null;
-    }
-
-    // Load member details
-    String memberDetailsPath = String.format("member/%s", memberSummary.id);
+  public LBMember fetch(String userLID) {
+    String memberDetailsPath = String.format("member/%s", userLID);
     ResponseEntity<LBMember> memberDetailsResponse =
         this.client.getAuthorized(memberDetailsPath, LBMember.class);
     if (memberDetailsResponse == null || memberDetailsResponse.getBody() == null) {
       return null;
     }
 
-    CombinedLBMemberModel combinedLBMemberModel = new CombinedLBMemberModel();
-    combinedLBMemberModel.member = memberDetailsResponse.getBody();
-    combinedLBMemberModel.memberSummary = memberSummary;
-
-    return combinedLBMemberModel;
+    return memberDetailsResponse.getBody();
   }
 }
