7f7b7c9d1878132e3d78d9e5f14be20d931897da
Jim Lind <spoon.vw@gmail.com>
2024-10-28 22:17:37 -0400

    feat: firestore creates and subscribes
    also comes with a reformat.
    oof.

--- src/main/java/jimlind/filmlinkd/system/google/FirestoreManager.java
+++ src/main/java/jimlind/filmlinkd/system/google/FirestoreManager.java
@@ -1,11 +1,8 @@
 package jimlind.filmlinkd.system.google;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.AggregateQuerySnapshot;
-import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.*;
 import com.google.cloud.firestore.FirestoreOptions.Builder;
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.cloud.firestore.QuerySnapshot;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +11,7 @@ import jimlind.filmlinkd.Config;
 import jimlind.filmlinkd.factory.UserFactory;
 import jimlind.filmlinkd.model.User;
 import jimlind.filmlinkd.system.letterboxd.LidComparer;
+import jimlind.filmlinkd.system.letterboxd.model.LBMember;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -36,6 +34,17 @@ public class FirestoreManager {
             .setProjectId(this.config.getGoogleProjectId());
     FirestoreOptions firestoreOptions = builder.build();
     this.db = firestoreOptions.getService();
+  }
+
+  public void createUserDocument(LBMember member) {
+    String collectionId = this.config.getFirestoreCollectionId();
+    User user = this.userFactory.createFromMember(member);
+
+    try {
+      this.db.collection(collectionId).document(user.id).set(user.toMap()).get();
+    } catch (Exception e) {
+      log.atError().setMessage("Unable to set user document").log();
+    }
   }
 
   public QueryDocumentSnapshot getUserDocument(String userLID) {
@@ -72,7 +81,45 @@ public class FirestoreManager {
     }
   }
 
-  public boolean updateUserSubscription(String userLID, String channelId) {
+  public boolean addUserSubscription(String userLID, String channelId) {
+    // Create user but also save snapshot to update with
+    QueryDocumentSnapshot snapshot = this.loadDocumentSnapshotByUserLID(userLID);
+    User user = this.userFactory.createFromSnapshot(snapshot);
+    if (snapshot == null || user == null) {
+      System.out.println("user doesn't exist");
+      return false;
+    }
+
+    ArrayList<User.Channel> channelList = user.channelList;
+
+    // If the channel is already subscribed then exit early positively
+    for (User.Channel channel : channelList) {
+      if (channel.channelId.equals(channelId)) {
+        return true;
+      }
+    }
+
+    User.Channel newChannel = new User.Channel();
+    newChannel.channelId = channelId;
+    user.channelList.add(newChannel);
+
+    // Perform the update
+    try {
+      snapshot.getReference().update(user.toMap());
+    } catch (Exception e) {
+      System.out.println(e);
+      log.atError()
+          .setMessage("Unable to Update Channel List: Update Failed")
+          .addKeyValue("user", user)
+          .addKeyValue("channelId", channelId)
+          .log();
+      return false;
+    }
+
+    return true;
+  }
+
+  public boolean removeUserSubscription(String userLID, String channelId) {
     // TODO: Fill this out
     return false;
   }
@@ -80,18 +127,13 @@ public class FirestoreManager {
   public boolean updateUserPrevious(
       String userLID, String diaryLID, long diaryPublishedDate, String diaryURI) {
 
-    QueryDocumentSnapshot snapshot;
-    try {
-      snapshot = getUserDocument(userLID);
-    } catch (Exception e) {
-      log.atWarn()
-          .setMessage("Unable to Update Previous: User Not Found")
-          .addKeyValue("userLID", userLID)
-          .log();
+    // Create user but also save snapshot to update with
+    QueryDocumentSnapshot snapshot = this.loadDocumentSnapshotByUserLID(userLID);
+    User user = this.userFactory.createFromSnapshot(snapshot);
+    if (snapshot == null || user == null) {
       return false;
     }
 
-    User user = this.userFactory.createFromSnapshot(snapshot);
     ArrayList<String> previousList = user.previous.list;
 
     // Nothing to update. Return `true` as if the action succeeded.
@@ -99,20 +141,20 @@ public class FirestoreManager {
       return true;
     }
 
+    // The previous list should be considered the primary source of truth
+    user.previous.list = LidComparer.buildMostRecentList(previousList, diaryLID, 10);
+
+    // The scraping process uses the previous lid as it's source of truth to determine which
+    // entries are new. Maybe that should be changed eventually.
+    user.previous.lid = user.getMostRecentPrevious();
+
+    // This data is only used if I'm trying to debug publishing issues
+    user.updated = Instant.now().toEpochMilli();
+    user.previous.published = diaryPublishedDate;
+    user.previous.uri = diaryURI;
+
+    // Perform the update
     try {
-      // The previous list should be considered the primary source of truth
-      user.previous.list = LidComparer.buildMostRecentList(previousList, diaryLID, 10);
-
-      // The scraping process uses the previous lid as it's source of truth to determine which
-      // entries are new. Maybe that should be changed eventually.
-      user.previous.lid = user.getMostRecentPrevious();
-
-      // This data is only used if I'm trying to debug publishing issues
-      user.updated = Instant.now().toEpochMilli();
-      user.previous.published = diaryPublishedDate;
-      user.previous.uri = diaryURI;
-
-      // Preform the update
       snapshot.getReference().update(user.toMap());
     } catch (Exception e) {
       log.atError()
@@ -124,5 +166,18 @@ public class FirestoreManager {
     }
 
     return true;
+  }
+
+  private QueryDocumentSnapshot loadDocumentSnapshotByUserLID(String userLID) {
+    QueryDocumentSnapshot snapshot = getUserDocument(userLID);
+    if (snapshot == null) {
+      log.atWarn()
+          .setMessage("Unable to Update Previous: User Not Found")
+          .addKeyValue("userLID", userLID)
+          .log();
+      return null;
+    }
+
+    return snapshot;
   }
 }
