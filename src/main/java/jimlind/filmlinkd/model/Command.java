b6ed59abd764f5fbfd6a3833db36c31411db0692
Jim Lind <spoon.vw@gmail.com>
2024-10-31 22:18:54 -0400

    feat: make that command model jsonable

--- src/main/java/jimlind/filmlinkd/model/Command.java
+++ src/main/java/jimlind/filmlinkd/model/Command.java
@@ -1,7 +1,13 @@
 package jimlind.filmlinkd.model;
 
+import com.google.gson.Gson;
+
 public class Command {
   public String command;
   public String user;
   public String entry;
+
+  public String toJson() {
+    return new Gson().toJson(this);
+  }
 }
